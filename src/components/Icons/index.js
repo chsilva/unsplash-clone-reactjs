18f3e228946b6c2e6a414b10f05450d1b72b2c5d
Caio Henrique Silva <caiohenriquesilva@live.com>
2019-05-31 17:34:31 -0300

    create spinner icon

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+import { SpinnerWrap } from "./styles";
 
 export const Bell = props => (
   <FontAwesomeIcon icon={faBell} className={`icon ${props.customClass}`} />
+);
+
+export const Spinner = props => (
+  <SpinnerWrap>
+    <FontAwesomeIcon
+      icon={faSpinner}
+      className={`icon ${props.customClass}`}
+      size={props.size}
+      spin
+    />
+  </SpinnerWrap>
 );
