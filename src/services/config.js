e4f9c9689c484757539b27a20feca23becdde591
Caio Henrique Silva <caiohenriquesilva@live.com>
2019-05-31 17:27:34 -0300

    create service config and home service class

---
+++ src/services/config.js
@@ -0,0 +1,6 @@
+const self = {};
+
+self.UNSPLASH_API_URL = "https://api.unsplash.com";
+self.UNSPLASH_CLIENT_ID = process.env.REACT_APP_UNSPLASH_CLIENT_ID;
+
+export default self;
---
+++ src/services/home.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import config from "./config";
+
+class HomeService {
+  static getPhotos(params = {}) {
+    const page = params.page || 1;
+    const perPage = params.perPage || 30;
+    return axios(
+      `${config.UNSPLASH_API_URL}/photos?client_id=${
+        config.UNSPLASH_CLIENT_ID
+      }&per_page=${perPage}&page=${page}`
+    );
+  }
+}
+
+export default HomeService;
