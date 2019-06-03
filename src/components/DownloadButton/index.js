ee562b65af8c7724528a0545e2229ff6ba3eeb0b
Caio Henrique Silva <caio@caiodev.com>
2019-06-03 01:54:35 -0300

    fix w3c security problem for the 'a' html tag

--- src/components/DownloadButton/index.js
+++ src/components/DownloadButton/index.js
@@ -4,7 +4,7 @@ import { ArrowDown } from "../Icons";
 
 const DownloadButton = props => {
   return (
-    <a href={props.download} download>
+    <a href={props.download} target="_blank" rel="noopener noreferrer" download>
       <Button
         customStyle={{ width: "40px", height: "32px", marginRight: "16px" }}
       >
