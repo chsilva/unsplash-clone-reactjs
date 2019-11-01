0db9f2e1c78b7c85a541b7b0bd659a21c7494d16
Caio Henrique Silva <caio@caiodev.com>
2019-11-01 15:59:48 -0300

    items per page to 15 from 30

--- src/services/home.js
+++ src/services/home.js
@@ -1,16 +1,14 @@
-import axios from "axios";
-import config from "./config";
+import axios from "axios"
+import config from "./config"
 
 class HomeService {
   static getPhotos(params = {}) {
-    const page = params.page || 1;
-    const perPage = params.perPage || 30;
+    const page = params.page || 1
+    const perPage = params.perPage || 15
     return axios(
-      `${config.UNSPLASH_API_URL}/photos?client_id=${
-        config.UNSPLASH_CLIENT_ID
-      }&per_page=${perPage}&page=${page}`
-    );
+      `${config.UNSPLASH_API_URL}/photos?client_id=${config.UNSPLASH_CLIENT_ID}&per_page=${perPage}&page=${page}`,
+    )
   }
 }
 
-export default HomeService;
+export default HomeService
